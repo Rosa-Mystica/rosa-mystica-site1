094c502d69299b701e3d1ba62e0a3ec713a5a189
Shivam Rai <shivamrai@Shivams-MacBook-Air.local>
2025-05-21 00:00:26 +0530

    🔧 Fixed blank page

--- Desktop/Rosa-Mystica-web/rosa-mystica-site/client/src/utils/axios.js
+++ Desktop/Rosa-Mystica-web/rosa-mystica-site/client/src/utils/axios.js
@@ -1,7 +1,8 @@
+// client/src/utils/axios.js
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // this pulls from .env
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default instance;
